843dbdf20ee15d75ce067f75e1f785334aa7ab54
Mohannad Najjar <mohannadnaj@me.com>
2020-05-28 18:02:18 +0300

    Modified Revert of commit 0f005fc5b8f1dcd95e2173d849bc98d36a3c0df3
    "Fix "Data too long for column 'description'""
    
    This reverts commit 0f005fc5b8f1dcd95e2173d849bc98d36a3c0df3.

--- app/TwUtils/TweepsManager.php
+++ app/TwUtils/TweepsManager.php
@@ -60,7 +60,7 @@ class TweepsManager
             'avatar'           => $user['profile_image_url_https'],
             'screen_name'      => $user['screen_name'],
             'location'         => $user['location'] ?? null,
-            'description'      => Str::limit($user['description'], 190) ?? null,
+            'description'      => Str::limit($user['description'], 254) ?? null,
             'url'              => $user['url'] ?? null,
             'display_url'      => $displayUrl ?? null,
             'followers_count'  => $user['followers_count'] ?? null,

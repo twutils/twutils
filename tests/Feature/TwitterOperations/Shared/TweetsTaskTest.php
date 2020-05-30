214291f7fa34789d2e1e74f15b955ba5a5304465
Mohannad Najjar <mohannadnaj@me.com>
2020-05-31 01:15:17 +0300

    Minimize twitter requests for custom dates by defining `since_id/max_id`
    Squashed commit of the following:
    
    commit badc70c0b47aaadf49e2fc196939120152f7b1c0
    Author: Mohannad Najjar <mohannadnaj@me.com>
    Date:   Sat May 30 00:54:19 2020 +0300
    
        Cleaning tests
    
    commit b134a195d1db2e4b3c8dd7621d08ffae009e87db
    Author: Mohannad Najjar <mohannadnaj@me.com>
    Date:   Sat May 30 00:30:08 2020 +0300
    
        WIP
    
    commit e9c56ec2088b79938d0caa24eaf07b9ee759068c
    Author: Mohannad Najjar <mohannadnaj@me.com>
    Date:   Sat May 30 00:21:19 2020 +0300
    
        WIP
    
    commit a9fc4d5cdf9fa83ad99a427f8ab56f42d3516021
    Author: Mohannad Najjar <mohannadnaj@me.com>
    Date:   Fri May 29 23:55:55 2020 +0300
    
        WIP

--- tests/Feature/TwitterOperations/Shared/TweetsTaskTest.php
+++ tests/Feature/TwitterOperations/Shared/TweetsTaskTest.php
@@ -8,6 +8,8 @@ use App\Tweep;
 use App\Tweet;
 use App\Jobs\CleanLikesJob;
 use App\Exports\TasksExport;
+use Tests\TwitterClientMock;
+use Illuminate\Support\Carbon;
 use Tests\IntegrationTestCase;
 use Illuminate\Support\Facades\Bus;
 use Maatwebsite\Excel\Facades\Excel;
@@ -19,6 +21,7 @@ abstract class TweetsTaskTest extends IntegrationTestCase
 {
     protected $jobName;
     protected $apiEndpoint;
+    protected $twitterEndpoint;
 
     public function test_basic_test()
     {
@@ -255,21 +258,14 @@ abstract class TweetsTaskTest extends IntegrationTestCase
         $this->withoutJobs();
         $this->logInSocialUser('api');
 
-        $tweet = $this->getStub('tweet.json');
+        $tweets = $this->generateUniqueTweets(2);
 
-        $this->bindTwitterConnector([$tweet, $tweet]);
+        $this->bindTwitterConnector($tweets);
 
         $this->getJson($this->apiEndpoint)
         ->assertStatus(200);
 
-        $this->fireJobsAndBindTwitter(
-            [
-                [
-                    'type' => CleanLikesJob::class,
-                    'skip' => true,
-                ],
-            ]
-        );
+        $this->fireJobsAndBindTwitter([]);
 
         $this->assertTaskCount(1, 'completed');
         $this->assertCount(2, Tweet::all());
@@ -306,12 +302,223 @@ abstract class TweetsTaskTest extends IntegrationTestCase
         $this->assertLikesBelongsToTask();
     }
 
+    public function test_save_likes_has_correct_twitter_parameters()
+    {
+        $this->withoutJobs();
+        $this->logInSocialUser('api');
+
+        config()->set(['twutils.minimum_expected_likes' => 2]);
+        config()->set(['twutils.twitter_requests_counts.fetch_likes' => 3]);
+
+        $tweets = $this->generateUniqueTweets(10);
+
+        $tweetsDividedForMultipleJobs = [];
+
+        collect($tweets)
+            ->chunk(config('twutils.twitter_requests_counts.fetch_likes'))
+            ->map(function($tweetsChunk) use (& $tweetsDividedForMultipleJobs) {
+
+                $tweetsDividedForMultipleJobs[] = [
+                    'type'           => $this->jobName,
+                    'twitterData'    => $tweetsChunk->toArray(),
+                ];
+            });
+
+        $tweetsDividedForMultipleJobs[] = [
+            'type'           => $this->jobName,
+            'twitterData'    => [],
+        ];
+
+        $this->getJson($this->apiEndpoint)
+        ->assertStatus(200);
+
+        $this->fireJobsAndBindTwitter($tweetsDividedForMultipleJobs);
+
+        $lastJobIndex = count($this->dispatchedJobs);
+
+        $this->getJson($this->apiEndpoint)
+        ->assertStatus(200);
+
+        $this->fireJobsAndBindTwitter($tweetsDividedForMultipleJobs, $lastJobIndex);
+
+        $this->assertTaskCount(2, 'completed');
+        $this->assertCount(10, Task::find(1)->likes->pluck('id_str'));
+        $this->assertCount(10, Task::find(2)->likes);
+
+        // Assert it's 8 Requests since we are requesting with parameter
+        // count as '3' tweets while it's all 10 tweets.
+        // So the 10 tweets will be fetched in 4 requests.
+        // and we have 2 tasks, so it's 8 requests.
+        $this->assertCount(
+            8,
+            TwitterClientMock::getAllCallsData(),
+        );
+
+        foreach (TwitterClientMock::getAllCallsData() as $index => $twitterCallData) {
+            $this->assertEquals(
+                $this->twitterEndpoint,
+                $twitterCallData['endpoint'],
+            );
+
+            $expectedParameters = $this->initalTwitterParametersKeys;
+
+            // 1st request and 5th are the first requests each was performed for a newly created task
+            // So only them, aren't expected to have 'max_id' parameter
+
+            if ( ! in_array($index, [0, 4])) {
+                $expectedParameters = array_merge($this->initalTwitterParametersKeys, ['max_id']);
+            }
+
+            $this->assertEquals(
+                $expectedParameters,
+                array_keys($twitterCallData['parameters']),
+                'Request [' . $index . '] to twitter doesn\'t contain the correct parameters'
+            );
+        }
+    }
+
+    public function test_save_likes_with_and_without_custom_date_all_has_correct_twitter_parameters()
+    {
+        $this->withoutJobs();
+        $this->logInSocialUser('api');
+
+        config()->set(['twutils.minimum_expected_likes' => 2]);
+        config()->set(['twutils.twitter_requests_counts.fetch_likes' => 3]);
+
+        $tweets = $this->generateUniqueTweets(30);
+
+        $tweetsDividedForMultipleJobs = [];
+
+        collect($tweets)
+            ->chunk(config('twutils.twitter_requests_counts.fetch_likes'))
+            ->map(function($tweetsChunk) use (& $tweetsDividedForMultipleJobs) {
+
+                $tweetsDividedForMultipleJobs[] = [
+                    'type'           => $this->jobName,
+                    'twitterData'    => $tweetsChunk->toArray(),
+                ];
+            });
+
+        $tweetsDividedForMultipleJobs[] = [
+            'type'           => $this->jobName,
+            'twitterData'    => [],
+        ];
+
+        $this->getJson($this->apiEndpoint)
+        ->assertStatus(200);
+
+        $this->fireJobsAndBindTwitter($tweetsDividedForMultipleJobs);
+
+        $lastJobIndex = count($this->dispatchedJobs);
+
+        $taskSettings = [
+            'start_date' => now()->subDays(7)->format('Y-m-d'),
+            'end_date'   => now()->subDays(3)->format('Y-m-d'),
+        ];
+
+        $this->postJson($this->apiEndpoint, [
+            'settings' => $taskSettings,
+        ])
+        ->assertStatus(200);
+
+        $tweetsDividedForMultipleJobs = [];
+
+        collect($tweets)
+            ->filter(function ($tweet) use ($taskSettings) {
+                    $shouldReturn = true;
+                    $tweetCreatedAt =  Carbon::createFromTimestamp(strtotime($tweet->created_at ?? 1));
+
+                    if (
+                        isset($taskSettings['start_date']) &&
+                        ! $tweetCreatedAt->greaterThanOrEqualTo($taskSettings['start_date'])
+                    ) {
+                        $shouldReturn = false;
+                    }
+
+                    if (
+                        isset($taskSettings['end_date']) &&
+                        ! $tweetCreatedAt->lessThanOrEqualTo($taskSettings['end_date'])
+                    ) {
+                        $shouldReturn = false;
+                    }
+
+                    return $shouldReturn;
+            })
+            ->chunk(config('twutils.twitter_requests_counts.fetch_likes'))
+            ->map(function($tweetsChunk) use (& $tweetsDividedForMultipleJobs) {
+
+                $tweetsDividedForMultipleJobs[] = [
+                    'type'           => $this->jobName,
+                    'twitterData'    => $tweetsChunk->toArray(),
+                ];
+            });
+
+        $tweetsDividedForMultipleJobs[] = [
+            'type'           => $this->jobName,
+            'twitterData'    => [],
+        ];
+
+        $this->fireJobsAndBindTwitter($tweetsDividedForMultipleJobs, $lastJobIndex);
+
+        $this->assertTaskCount(2, 'completed');
+        $this->assertCount(30, Task::find(1)->likes->pluck('id_str'));
+        $this->assertCount(4, Task::find(2)->likes);
+
+        // Assert it's 6 Requests since we are requesting with parameter
+        // count as '3' tweets while it's all 10 tweets.
+        // So the first task 10 tweets will be fetched in 4 requests.
+        // And the second task is parameterized to fetch only 
+        // within 4 tweets that will be fetched in 3 requests.
+        $this->assertCount(
+            13,
+            TwitterClientMock::getAllCallsData(),
+        );
+
+        $initialMaxIdForSettingsTask = null;
+
+        foreach (TwitterClientMock::getAllCallsData() as $index => $twitterCallData) {
+            $this->assertEquals(
+                $this->twitterEndpoint,
+                $twitterCallData['endpoint'],
+            );
+
+            $expectedParameters = $this->initalTwitterParametersKeys;
+
+            // 1st request and 11th are the first requests each was performed for a newly created task
+            // So only them, aren't expected to have 'max_id' parameter
+
+            if ( $index !== 0 && $index < 11) {
+                $expectedParameters = array_merge($this->initalTwitterParametersKeys, ['max_id']);
+            } else if ($index >= 11) {
+                $expectedParameters = array_merge($this->initalTwitterParametersKeys, ['since_id', 'max_id']);
+            }
+
+            $this->assertEquals(
+                $expectedParameters,
+                array_keys($twitterCallData['parameters']),
+                'Request [' . $index . '] to twitter doesn\'t contain the correct parameters.'
+                . json_encode($twitterCallData)
+            );
+
+            if ($index == 11)
+            {
+                $initialMaxIdForSettingsTask = $twitterCallData['parameters']['max_id'];
+            }
+
+            if ($index > 11)
+            {
+                $this->assertNotNull($initialMaxIdForSettingsTask);
+                $this->assertNotEquals($initialMaxIdForSettingsTask, $twitterCallData['parameters']['max_id']);
+            }
+        }
+    }
+
     public function test_basic_save_tweets_first_request_has_error()
     {
         $this->withoutJobs();
         $this->logInSocialUser('api');
 
-        $tweet = $this->getStub('tweet.json');
+        $tweets = $this->generateUniqueTweets(2);
 
         $this->getJson($this->apiEndpoint)
         ->assertStatus(200);
@@ -335,16 +542,12 @@ abstract class TweetsTaskTest extends IntegrationTestCase
             ],
             [
                 'type'        => $this->jobName,
-                'twitterData' => array_fill(0, 2, $tweet),
+                'twitterData' => $tweets,
                 'after'       => function ($job) {
                     $this->assertNotNull($job->delay);
                     $nextJobDelay = $this->dispatchedJobs[1]->delay->diffInSeconds(now());
                     $this->assertLessThanOrEqual(60, $nextJobDelay);
                 },
-            ],
-            [
-                'type' => CleanLikesJob::class,
-                'skip' => true,
             ],
         ]);
 
@@ -358,7 +561,7 @@ abstract class TweetsTaskTest extends IntegrationTestCase
         $this->withoutJobs();
         $this->logInSocialUser('api');
 
-        $tweet = $this->getStub('tweet.json');
+        $tweets = collect($this->generateUniqueTweets(25))->chunk(10)->map->toArray();
 
         config()->set(['twutils.minimum_expected_likes' => 10]);
 
@@ -369,19 +572,15 @@ abstract class TweetsTaskTest extends IntegrationTestCase
             [
                 [
                     'type'        => $this->jobName,
-                    'twitterData' => array_fill(0, 10, $tweet),
+                    'twitterData' => $tweets[0],
                 ],
                 [
                     'type'        => $this->jobName,
-                    'twitterData' => array_fill(0, 10, $tweet),
+                    'twitterData' => $tweets[1],
                 ],
                 [
                     'type'        => $this->jobName,
-                    'twitterData' => array_fill(0, 5, $tweet),
-                ],
-                [
-                    'type' => CleanLikesJob::class,
-                    'skip' => true,
+                    'twitterData' => $tweets[2],
                 ],
             ]
         );
@@ -397,7 +596,7 @@ abstract class TweetsTaskTest extends IntegrationTestCase
         $this->withoutJobs();
         $this->logInSocialUser('api');
 
-        $tweet = $this->getStub('tweet.json');
+        $tweets = collect($this->generateUniqueTweets(20))->chunk(10)->map->toArray();
         $expiredTokenStub = $this->getStub('expired_token.json');
 
         config()->set(['twutils.minimum_expected_likes' => 10]);
@@ -409,7 +608,7 @@ abstract class TweetsTaskTest extends IntegrationTestCase
             [
                 [
                     'type'        => $this->jobName,
-                    'twitterData' => array_fill(0, 10, $tweet),
+                    'twitterData' => $tweets[0],
                 ],
                 [
                     'type'        => $this->jobName,
@@ -417,15 +616,11 @@ abstract class TweetsTaskTest extends IntegrationTestCase
                 ],
                 [
                     'type'        => $this->jobName,
-                    'twitterData' => array_fill(0, 10, $tweet),
+                    'twitterData' => $tweets[1],
                 ],
                 [
                     'type'        => $this->jobName,
                     'twitterData' => [],
-                ],
-                [
-                    'type' => CleanLikesJob::class,
-                    'skip' => true,
                 ],
             ]
         );
@@ -441,7 +636,7 @@ abstract class TweetsTaskTest extends IntegrationTestCase
         $this->withoutJobs();
         $this->logInSocialUser('api');
 
-        $tweet = $this->getStub('tweet.json');
+        $tweets = collect($this->generateUniqueTweets(20))->chunk(10)->map->toArray();
 
         config()->set(['twutils.minimum_expected_likes' => 10]);
 
@@ -452,19 +647,15 @@ abstract class TweetsTaskTest extends IntegrationTestCase
             [
                 [
                     'type'        => $this->jobName,
-                    'twitterData' => array_fill(0, 10, $tweet),
+                    'twitterData' => $tweets[0],
                 ],
                 [
                     'type'        => $this->jobName,
-                    'twitterData' => array_fill(0, 10, $tweet),
+                    'twitterData' => $tweets[1],
                 ],
                 [
                     'type'        => $this->jobName,
                     'twitterData' => [],
-                ],
-                [
-                    'type' => CleanLikesJob::class,
-                    'skip' => true,
                 ],
             ]
         );
@@ -658,10 +849,6 @@ abstract class TweetsTaskTest extends IntegrationTestCase
                     'type'        => $this->jobName,
                     'twitterData' => [],
                 ],
-                [
-                    'type' => CleanLikesJob::class,
-                    'skip' => false,
-                ],
             ]
         );
 
@@ -712,10 +899,6 @@ abstract class TweetsTaskTest extends IntegrationTestCase
                 [
                     'type'        => $this->jobName,
                     'twitterData' => [],
-                ],
-                [
-                    'type' => CleanLikesJob::class,
-                    'skip' => false,
                 ],
             ]
         );

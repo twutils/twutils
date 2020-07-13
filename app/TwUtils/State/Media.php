0b9283aeaaa4d548d2f0ae53efb7cf49046b7cbe
Mohannad Najjar <mohannadnaj@me.com>
2020-07-13 05:13:07 +0300

    ok

--- app/TwUtils/State/Media.php
+++ app/TwUtils/State/Media.php
@@ -5,22 +5,12 @@ namespace App\TwUtils\State;
 class Media
 {
 
-    protected $data;
-    protected $type;
+    public $data;
+    public $type;
 
     public function __construct($data, $type = '')
     {
         $this->data = $data;
         $this->type = $type;
-    }
-
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    public function getPath()
-    {
-        return $this->data;
     }
 }

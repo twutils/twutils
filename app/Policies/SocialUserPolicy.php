4fb952f71cb2f72681ae7aace04ebd9b3ceaee24
Mohannad Najjar <mohannadnaj@me.com>
2021-01-30 09:34:29 +0300

    Cleaning: Remove unused code

--- app/Policies/SocialUserPolicy.php
+++ app/Policies/SocialUserPolicy.php
@@ -10,80 +10,8 @@ class SocialUserPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the social user.
-     *
-     * @param \App\Models\User       $user
-     * @param \App\Models\SocialUser $socialUser
-     *
-     * @return mixed
-     */
-    public function view(User $user, SocialUser $socialUser)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create social users.
-     *
-     * @param \App\Models\User $user
-     *
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the social user.
-     *
-     * @param \App\Models\User       $user
-     * @param \App\Models\SocialUser $socialUser
-     *
-     * @return mixed
-     */
     public function update(User $user, SocialUser $socialUser)
     {
         return $user->socialUsers->pluck('id')->contains($socialUser->id);
-    }
-
-    /**
-     * Determine whether the user can delete the social user.
-     *
-     * @param \App\Models\User       $user
-     * @param \App\Models\SocialUser $socialUser
-     *
-     * @return mixed
-     */
-    public function delete(User $user, SocialUser $socialUser)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the social user.
-     *
-     * @param \App\Models\User       $user
-     * @param \App\Models\SocialUser $socialUser
-     *
-     * @return mixed
-     */
-    public function restore(User $user, SocialUser $socialUser)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the social user.
-     *
-     * @param \App\Models\User       $user
-     * @param \App\Models\SocialUser $socialUser
-     *
-     * @return mixed
-     */
-    public function forceDelete(User $user, SocialUser $socialUser)
-    {
-        //
     }
 }
--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -10,26 +10,11 @@ class TaskPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the task.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Task $task
-     *
-     * @return mixed
-     */
     public function view(User $user, Task $task)
     {
         return $user->socialUsers->pluck('id')->contains($task->socialuser_id);
     }
 
-    /**
-     * Determine whether the user can create tasks.
-     *
-     * @param \App\Models\User $user
-     *
-     * @return mixed
-     */
     public function create(User $user)
     {
         return config('twutils.tasks_limit_per_user')
@@ -42,27 +27,11 @@ class TaskPolicy
             ->count();
     }
 
-    /**
-     * Determine whether the user can update the task.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Task $task
-     *
-     * @return mixed
-     */
     public function seeManagedTasks(User $user, Task $task)
     {
         return $this->view($user, $task);
     }
 
-    /**
-     * Determine whether the user can delete the task.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Task $task
-     *
-     * @return mixed
-     */
     public function delete(User $user, Task $task)
     {
         return $task->managed_by_task_id === null && $this->view($user, $task);
